1819a7d9d47a3cf5c42321d1fa1824836317db5e
Arushi Nagmote <144057243+arushinagmote@users.noreply.github.com>
2023-09-06 17:01:47 +0530

    Update VarEx1.java

--- VarEx1.java
+++ VarEx1.java
@@ -1,1 +1,13 @@
-
+class VarEx1 {
+    public static void main(String[] args) 
+    {
+        int rollno=10;
+        int age=221;
+        double price=1105.55;
+        boolean isFree=true;
+        
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isFree);
+    }
+}
